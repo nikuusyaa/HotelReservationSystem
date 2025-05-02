3123e7e230d39b99702382c97ee4690e6ac30354
Borozents Veronika <xborozv00@vutbr.cz>
2025-05-02 18:58:22 +0200

    fix: update react-router-dom and typescript versions in package files

--- src/App.js
+++ src/App.js
@@ -1,43 +1,51 @@
+// App.js
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 import BookRoom from "./components/BookRoom/components/BookRoomPage";
 import { AppBar, Toolbar, Typography, Button, Tooltip } from "@mui/material";
-
-//icons
 import LoginIcon from "@mui/icons-material/Login";
 
 function App() {
   return (
-    <>
-      <AppBar position="fixed" color="primary" elevation={4}>
-        <Toolbar>
-          <Typography
-            fontSize={50}
-            sx={{
-              flexGrow: 1,
-              textAlign: "center",
-              fontWeight: 700,
-              letterSpacing: "0.15em",
-            }}
-          >
-            NIAS HOTEL
-          </Typography>
+    <div className="App">
+      <header className="App-header">
+        <BrowserRouter>
+          <AppBar position="fixed" color="primary" elevation={4}>
+            <Toolbar>
+              <Typography
+                fontSize={50}
+                sx={{
+                  flexGrow: 1,
+                  textAlign: "center",
+                  fontWeight: 700,
+                  letterSpacing: "0.15em",
+                }}
+              >
+                NIAS HOTEL
+              </Typography>
+              <Tooltip title="Login">
+                <Button
+                  color="inherit"
+                  onClick={() => (window.location.href = "/login")}
+                >
+                  <LoginIcon />
+                </Button>
+              </Tooltip>
+            </Toolbar>
+          </AppBar>
 
-          <Tooltip title="Login">
-            <Button
-              color="inherit"
-              onClick={() => (window.location.href = "/login")}
-            >
-              <LoginIcon />
-            </Button>
-          </Tooltip>
-        </Toolbar>
-      </AppBar>
-      <div className="App">
-        <header className="App-header">
-          <BookRoom />
-        </header>
-      </div>
-    </>
+          <Toolbar />
+
+          <Routes>
+            <Route path="/" element={<Navigate to="/booking" replace />} />
+
+            <Route path="/booking" element={<BookRoom />} />
+
+            <Route path="*" element={<Navigate to="/booking" replace />} />
+          </Routes>
+        </BrowserRouter>
+      </header>
+    </div>
   );
 }
 
