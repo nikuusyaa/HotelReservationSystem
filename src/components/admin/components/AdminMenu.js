1d6d5254b4d947389cc2b54dcb454402acdf2a05
Borozents Veronika <xborozv00@vutbr.cz>
2025-05-04 18:19:41 +0200

    feat: add AdminMenu component and integrate it into AdminPage

---
+++ src/components/admin/components/AdminMenu.js
@@ -0,0 +1,66 @@
+import { IconButton, ListItemButton, Popover, Typography } from "@mui/material";
+
+import { useState } from "react";
+//icons
+import MenuIcon from "@mui/icons-material/Menu";
+import PaymentIcon from "@mui/icons-material/Payment";
+import HotelIcon from "@mui/icons-material/Hotel";
+import RoomServiceIcon from "@mui/icons-material/RoomService";
+import { Stack } from "@mui/system";
+import { blue, blueGrey, grey } from "@mui/material/colors";
+
+export default function AdminMenu() {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  return (
+    <>
+      <IconButton sx={{ color: blueGrey[900] }}>
+        <MenuIcon
+          sx={{ fontSize: 30 }}
+          variant="contained"
+          onClick={handleClick}
+        />
+      </IconButton>
+      <Popover
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "left",
+        }}
+      >
+        <Stack alignItems="flex-start">
+          <ListItemButton sx={{ width: "100%", color: blueGrey[800] }}>
+            <PaymentIcon />
+            <Typography fontSize={15} fontWeight={600}>
+              View Payments
+            </Typography>
+          </ListItemButton>
+
+          <ListItemButton sx={{ width: "100%", color: blueGrey[800] }}>
+            <HotelIcon />
+            <Typography fontSize={15} fontWeight={600}>
+              View Reservations
+            </Typography>
+          </ListItemButton>
+          <ListItemButton sx={{ width: "100%", color: blueGrey[800] }}>
+            <RoomServiceIcon />
+            <Typography fontSize={15} fontWeight={600}>
+              View Services Requests
+            </Typography>
+          </ListItemButton>
+        </Stack>
+      </Popover>
+    </>
+  );
+}
--- src/components/admin/components/AdminPage.js
+++ src/components/admin/components/AdminPage.js
@@ -1,5 +1,6 @@
 import { AppBar, Toolbar, Typography } from "@mui/material";
 import RequestsList from "./RequestsList";
+import AdminMenu from "./AdminMenu";
 
 export default function AdminPage() {
   return (
@@ -7,17 +8,25 @@ export default function AdminPage() {
       <AppBar
         position="fixed"
         color="default"
-        elevation={5}
+        elevation={10}
         sx={{
           top: 75,
           left: 0,
           right: 0,
         }}
       >
-        <Toolbar variant="dense">
-          <Typography fontSize={30} fontWeight={900}>
+        <Toolbar
+          variant="dense"
+          sx={{
+            display: "flex",
+            justifyContent: "space-between", 
+            alignItems: "center",
+          }}
+        >
+          <Typography sx={{ pl: 2 }} fontSize={30} fontWeight={900}>
             Hello, You are logged in as Administrator !
           </Typography>
+          <AdminMenu />
         </Toolbar>
         <RequestsList />
       </AppBar>
